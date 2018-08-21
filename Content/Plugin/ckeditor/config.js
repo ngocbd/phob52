cbba2c4010e86a06c2e2d567037edf47e5f67260
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-08-21 15:51:20 +0700

    change password please

---
+++ Content/Plugin/ckeditor/config.js
@@ -0,0 +1,22 @@
+﻿/*
+Copyright (c) 2003-2012, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+
+CKEDITOR.editorConfig = function( config )
+{
+	// Define changes to default configuration here. For example:
+	// config.language = 'fr';
+    // config.uiColor = '#AADC6E';
+    config.enterMode = CKEDITOR.ENTER_BR;
+    config.toolbar = 'Full';
+    config.filebrowserBrowseUrl = '/Content/Plugin/ckfinder/ckfinder.html';
+    config.filebrowserImageBrowseUrl = '/Content/Plugin/ckfinder/ckfinder.html?type=Images';
+    config.filebrowserFlashBrowseUrl = '/Content/Plugin/ckfinder/ckfinder.html?type=Flash';
+    config.filebrowserUploadUrl = '/Content/ckfinder/Plugin/core/connector/aspx/connector.aspx?command=QuickUpload&type=Files';
+    config.filebrowserImageUploadUrl = '/Content/Plugin/ckfinder/core/connector/aspx/connector.aspx?command=QuickUpload&type=Images';
+    config.filebrowserFlashUploadUrl = '/Content/Plugin/ckfinder/core/connector/aspx/connector.aspx?command=QuickUpload&type=Flash';
+    config.filebrowserWindowWidth = '700';
+    config.filebrowserWindowHeight = '1000';
+    
+};
